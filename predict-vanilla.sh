b2447309bb22cad4d6499c3075a29f6e1df9bb81
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2017-12-12 16:39:51 +0200

    Changes to driver scripts.

--- predict-vanilla.sh
+++ predict-vanilla.sh
@@ -5,6 +5,14 @@
 #SBATCH -t 10:00:00
 #SBATCH --gres gpu:1
 
+if [ $# -lt 1 ]
+then
+	echo "Usage: $0 exp [args...]" 1>&2
+	exit 1
+fi
+exp=$1
+shift
+
 module load cuda cudnn
 
 echo CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES
@@ -16,10 +24,13 @@ th=$USERAPPL/torch/install/bin/th
 export LD_LIBRARY_PATH=$CUDALIB
 
 modeldir=$WRKDIR/nn_coref-models
+bpsdir=bps
 
 cd $HOME/coref/nn_coref/nn
-$th vanilla_mr.lua -gpuid $CUDA_VISIBLE_DEVICES -Ha 128 -loadAndPredict \
-	-pwDevFeatPrefix test_small -anaDevFeatPrefix test_small \
-	-savedPWNetFi $modeldir/simple-vanilla-700-128.model-vanilla-pw \
-	-savedNANetFi $modeldir/simple-vanilla-700-128.model-vanilla-na
+$th vanilla_mr.lua -gpuid 0 -Ha 128 -loadAndPredict \
+	-pwDevFeatPrefix dev_small -anaDevFeatPrefix dev_small \
+	-savedPWNetFi $modeldir/$exp-vanilla-700-128.model-vanilla-pw \
+	-savedNANetFi $modeldir/$exp-vanilla-700-128.model-vanilla-na \
+	-bpfi $bpsdir/$exp-vanilla-700-128.bps \
+	"$@"
 
